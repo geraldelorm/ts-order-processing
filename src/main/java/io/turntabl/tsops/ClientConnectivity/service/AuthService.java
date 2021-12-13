e754676e845b60a7fa7337eccb4073a41f1c1087
ahadziii <ahadzi.airdem@gmail.com>
2021-12-13 19:43:19 +0000

    refactoring

--- src/main/java/io/turntabl/tsops/ClientConnectivity/service/AuthService.java
+++ src/main/java/io/turntabl/tsops/ClientConnectivity/service/AuthService.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
